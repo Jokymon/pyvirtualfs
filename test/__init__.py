cfbd1c849f2b8585980efb810e21e6be454018d0
Silvan Wegmann <swegmann@narf.ch>
2013-06-07 22:57:59 +0200

    Convert project for py.test usage

--- test/__init__.py
+++
@@ -1,8 +0,0 @@
-import os, os.path
-module_directory = os.path.dirname( __file__ )
-
-modules = []
-for filename in os.listdir( module_directory ):
-    if filename.endswith(".py") and filename != "__init__.py":
-        modules.append( filename[:-3] )
-__all__ = modules[:]
--- test/test_image_path_parser.py
+++ test/test_image_path_parser.py
@@ -1,74 +1,71 @@
+import pytest
 import image_path
-import unittest
 
-class TestImagePathParser(unittest.TestCase):
+class TestImagePathParser:
     def testValidFullPath(self):
         path_string = "image://imagefile.img:hd/partition0/directory/somefile.dat"
  
         path = image_path.ImagePath.parse(path_string)
  
-        self.assert_( path.imagefile == "imagefile.img" )
-        self.assert_( path.imagetype == image_path.ImagePath.IMAGE_TYPE_HD )
-        self.assert_( path.partition == 0 )
-        self.assert_( path.filepath  == "/directory/somefile.dat" )
+        assert path.imagefile == "imagefile.img"
+        assert path.imagetype == image_path.ImagePath.IMAGE_TYPE_HD
+        assert path.partition == 0
+        assert path.filepath  == "/directory/somefile.dat"
  
     def testEmptyPath(self):
         path_string = "image://imagefile.img:hd"
  
         path = image_path.ImagePath.parse(path_string)
  
-        self.assert_( path.imagefile == "imagefile.img" )
-        self.assert_( path.imagetype == image_path.ImagePath.IMAGE_TYPE_HD )
-        self.assert_( path.partition == None )
-        self.assert_( path.filepath  == "/" )
+        assert path.imagefile == "imagefile.img"
+        assert path.imagetype == image_path.ImagePath.IMAGE_TYPE_HD
+        assert path.partition == None
+        assert path.filepath  == "/"
  
     def testValidPathWithoutPartition(self):
         path_string = "image://imagefile.img:hd/directory/somefile.dat"
  
         path = image_path.ImagePath.parse(path_string)
  
-        self.assert_( path.imagefile == "imagefile.img" )
-        self.assert_( path.imagetype == image_path.ImagePath.IMAGE_TYPE_HD )
-        self.assert_( path.partition == None )
-        self.assert_( path.filepath  == "/directory/somefile.dat" )
+        assert path.imagefile == "imagefile.img"
+        assert path.imagetype == image_path.ImagePath.IMAGE_TYPE_HD
+        assert path.partition == None
+        assert path.filepath  == "/directory/somefile.dat"
  
     def testFloppyDiskImage(self):
         path_string = "image://imagefile.img:fd/directory/somefile.dat"
  
         path = image_path.ImagePath.parse(path_string)
  
-        self.assert_( path.imagefile == "imagefile.img" )
-        self.assert_( path.imagetype == image_path.ImagePath.IMAGE_TYPE_FD )
-        self.assert_( path.partition == None )
-        self.assert_( path.filepath  == "/directory/somefile.dat" )
+        assert path.imagefile == "imagefile.img"
+        assert path.imagetype == image_path.ImagePath.IMAGE_TYPE_FD
+        assert path.partition == None
+        assert path.filepath  == "/directory/somefile.dat"
 
     def testCdromImage(self):
         path_string = "image://imagefile.img:cdrom/directory/somefile.dat"
  
         path = image_path.ImagePath.parse(path_string)
  
-        self.assert_( path.imagefile == "imagefile.img" )
-        self.assert_( path.imagetype == image_path.ImagePath.IMAGE_TYPE_CDROM )
-        self.assert_( path.partition == None )
-        self.assert_( path.filepath  == "/directory/somefile.dat" )
+        assert path.imagefile == "imagefile.img"
+        assert path.imagetype == image_path.ImagePath.IMAGE_TYPE_CDROM
+        assert path.partition == None
+        assert path.filepath  == "/directory/somefile.dat"
 
     ################ FAILING CASES ##############################
  
     def testUnsupportedScheme(self):
         path_string = "http://imagefile.img:hd/directory/somefile.dat"
  
-        self.assertRaises( image_path.PathParseError, image_path.ImagePath.parse, path_string )
+        pytest.raises( image_path.PathParseError, image_path.ImagePath.parse, path_string )
 
     def testMissingImageType(self):
         path_string = "image://imagefile.img/directory/somefile.dat"
  
-        self.assertRaises( image_path.PathParseError, image_path.ImagePath.parse, path_string )
+        pytest.raises( image_path.PathParseError, image_path.ImagePath.parse, path_string )
         
 
     def testUnsupportedImageType(self):
         path_string = "image://imagefile.img:some_image_type/directory/somefile.dat"
  
-        self.assertRaises( image_path.PathParseError, image_path.ImagePath.parse, path_string )
- 
-if __name__ == "__main__":
-    unittest.main()
+        pytest.raises( image_path.PathParseError, image_path.ImagePath.parse, path_string )
