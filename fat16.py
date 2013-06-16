057636505e7e72bfea7258ad187b220843934116
Silvan Wegmann <swegmann@narf.ch>
2013-06-16 19:03:26 +0200

    Switch to usage of structures in FAT16 handler

--- fat16.py
+++ fat16.py
@@ -1,9 +1,17 @@
 import filesystem
 import os, sys
 from tools import *
+import structures
 
 #####################################################################################################
 # Implementation of the FAT16 specific classes
+
+class FAT16DirectoryEntry(structures.StructTemplate):
+    basename      = structures.StringField(0, 8)
+    suffix        = structures.StringField(8, 3)
+    attributes    = structures.UInt8Field(11)
+    start_cluster = structures.UInt16Field(26)
+    size          = structures.UInt32Field(28)
 
 class FAT16FileInfo(filesystem.FileInfo):
     def __init__(self):
@@ -28,7 +36,7 @@ class FAT16FileInfo(filesystem.FileInfo):
             self.attributes.append("archive")
         # determine the start byte of this file entry
         self.start_cluster = list2word(fat16_entry[26:28]) 
-        self.start_byte = (self.start_cluster-2) * fat16.sectors_per_cluster * 512
+        self.start_byte = (self.start_cluster-2) * fat16.info.sectors_per_cluster * 512
         self.start_byte += fat16.start_of_data
 
         self.size = list2dword(fat16_entry[28:32])
@@ -54,23 +62,23 @@ class FAT16FileHandle(filesystem.FileHandle):
 
         fat16 = self._fat16
         data = ""
-        pos_in_cluster = self._currentpos % (fat16.sectors_per_cluster*512)
-        cluster_index = self._currentpos / (fat16.sectors_per_cluster*512)
+        pos_in_cluster = self._currentpos % (fat16.info.sectors_per_cluster*512)
+        cluster_index = self._currentpos / (fat16.info.sectors_per_cluster*512)
         # TODO: also add an upper bound for the actual size of the file
         # Here we only read as much data until the boundary of the current cluster
-        readsize = min( fat16.sectors_per_cluster*512 - pos_in_cluster, size )
+        readsize = min( fat16.info.sectors_per_cluster*512 - pos_in_cluster, size )
         while readsize != 0:
-            start_byte = (self._clusters[cluster_index]-2) * fat16.sectors_per_cluster * 512
+            start_byte = (self._clusters[cluster_index]-2) * fat16.info.sectors_per_cluster * 512
             start_byte += fat16.start_of_data
             data += fat16[ start_byte : start_byte + readsize ]
 
             # update the current position
             self._currentpos += readsize
-            pos_in_cluster = self._currentpos % (fat16.sectors_per_cluster*512)
-            cluster_index = self._currentpos / (fat16.sectors_per_cluster*512)
+            pos_in_cluster = self._currentpos % (fat16.info.sectors_per_cluster*512)
+            cluster_index = self._currentpos / (fat16.info.sectors_per_cluster*512)
             # calculate the next chunk
             size -= readsize
-            readsize = min( fat16.sectors_per_cluster*512 - pos_in_cluster, size )
+            readsize = min( fat16.info.sectors_per_cluster*512 - pos_in_cluster, size )
 
         return data
 
@@ -79,25 +87,29 @@ class FAT16FileHandle(filesystem.FileHandle):
 
     def write(self, s):
         pass
+
+class FAT16Structure(structures.StructTemplate):
+    jump_code = structures.RawField(0, 3)
+    oem_name  = structures.StringField(3, 8)
+    bytes_per_sector        = structures.UInt16Field(11)
+    sectors_per_cluster     = structures.UInt8Field(13)
+    reserved_sectors        = structures.UInt16Field(14)
+    number_of_fats          = structures.UInt8Field(16)
+    max_root_dir_entries    = structures.UInt16Field(17)
+    sectors_per_fat         = structures.UInt16Field(22)
     
 class FAT16Filesystem:
     def __init__(self, partition):
         self.partition = partition
 
-        self.oem_name             = self.partition[3:11]
-        self.bytes_per_sector     = list2word(self.partition[11:13])
-        self.sectors_per_cluster  = self.partition[13]
-        self.reserved_sectors     = list2word(self.partition[14:16])
-        self.number_of_fats       = self.partition[16]
-        self.max_root_dir_entries = list2word(self.partition[17:19])
-        self.sectors_per_fat      = list2word(self.partition[22:24])
+        self.info = FAT16Structure(self.partition, 0)
 
         # calculate the byte address of cluster 2
-        self.start_of_data = 512 * (self.number_of_fats*self.sectors_per_fat + 1 ) # root directory start
-        self.start_of_data += self.max_root_dir_entries*32 # add size of root directory
+        self.start_of_data = 512 * (self.info.number_of_fats*self.info.sectors_per_fat + 1 ) # root directory start
+        self.start_of_data += self.info.max_root_dir_entries*32 # add size of root directory
 
         self.root_entries = {}
-        root_directory_start = 512 * ( self.number_of_fats*self.sectors_per_fat+1 )
+        root_directory_start = 512 * ( self.info.number_of_fats*self.info.sectors_per_fat+1 )
         i = 0
         fi = FAT16FileInfo()
         fi.parse_entry(self, self.partition[root_directory_start + i*32 : root_directory_start + (i+1)*32])
@@ -110,17 +122,17 @@ class FAT16Filesystem:
     def dump(self, fd=sys.stdout):
         fd.write("Content of the FAT:\n")
 
-        fd.write("OEM Name: %s\n" % self.oem_name)
-        fd.write("Bytes per sector: %u\n" % self.bytes_per_sector)
-        fd.write("Sectors per cluster: %u\n" % self.sectors_per_cluster)
-        fd.write("Reserved sectors: %u\n" % self.reserved_sectors)
-        fd.write("Number of FATs: %u\n" % self.number_of_fats)
-        fd.write("Sectors per FAT: %u\n" % self.sectors_per_fat)
-        fd.write("Maximum number of root directory entries: %u\n" % self.max_root_dir_entries)
+        fd.write("OEM Name: %s\n" % self.info.oem_name)
+        fd.write("Bytes per sector: %u\n" % self.info.bytes_per_sector)
+        fd.write("Sectors per cluster: %u\n" % self.info.sectors_per_cluster)
+        fd.write("Reserved sectors: %u\n" % self.info.reserved_sectors)
+        fd.write("Number of FATs: %u\n" % self.info.number_of_fats)
+        fd.write("Sectors per FAT: %u\n" % self.info.sectors_per_fat)
+        fd.write("Maximum number of root directory entries: %u\n" % self.info.max_root_dir_entries)
         fd.write("Start byte of data: %u\n" % self.start_of_data)
 
     def get_fat_entry(self, fat_no = 0, fat_entry = 0):
-        fat_start = 512*self.reserved_sectors + fat_no*(512*self.sectors_per_fat)
+        fat_start = 512*self.reserved_sectors + fat_no*(512*self.info.sectors_per_fat)
         return list2word( self.partition[ fat_start + 2*fat_entry : fat_start + 2*fat_entry + 2] )
 
     def listdir(self, directory):
