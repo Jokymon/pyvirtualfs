60dcad80d166b7c9e233f64990de8dcd6e4f865c
Silvan Wegmann <swegmann@narf.ch>
2013-10-09 22:49:27 +0200

    Git first tests for command tools

---
+++ test/test_command_tools.py
@@ -0,0 +1,11 @@
+from command_tools import *
+
+class TestCommandTools:
+    def test_command_wrapper(self):
+        @command
+        def wrapped_function():
+            """Testdocumentation of command {cmd}"""
+            pass
+
+        assert iscommand(wrapped_function)
+        assert wrapped_function.__doc__ == "Testdocumentation of command wrapped_function"
